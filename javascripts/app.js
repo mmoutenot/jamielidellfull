145d03923975f363c000ff2092a403beb25ff947
Marshall <mmoutenot@gmail.com>
2013-01-05 01:41:36 -0600

    add volume control via scroll position. add color modulating album cover.

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,11 +1,14 @@
 $(document).ready(function (){
   var refreshID = setInterval(function(){
     var currentHoleSize = 1.9;
+    var volume = 100;
     var scrollPosition = $(document).scrollTop();
     currentHoleSize = Math.max(1.9 - scrollPosition/190, 0.5);
+    volume = 100 - $(document).scrollTop()/7;
     $('#page').css({'background-image':
       '-webkit-radial-gradient(rgba(10, 10, 10, .1) '+currentHoleSize+'px, #111 1px)'});
-  }, 30);
+    window.player.setVolume(volume);
+  }, 100);
 });
 
 $('#title').click(function (){
@@ -60,4 +63,69 @@ $(window).resize(function() {
 }).resize();
 
 })
+
+var hue_val = 0;
+var iid = null;
+
+if ( $.browser.webkit ) {
+  $('#albumcover').live('mouseenter', function() {
+      this.iid = setInterval(function() {
+        $("#jamieimg").css("-webkit-filter","hue-rotate("+hue_val+"deg)");
+        $("#jamieimg").css("-moz-filter","hue-rotate("+hue_val+"deg)");
+        $("#jamieimg").css("-o-filter","hue-rotate("+hue_val+"deg)");
+        $("#jamieimg").css("-ms-filter","hue-rotate("+hue_val+"deg)");
+        $("#jamieimg").css("filter","hue-rotate("+hue_val+"deg)");
+        hue_val = (hue_val + 8);
+        }, 10);
+      }).live('mouseout', function(){
+        this.iid && clearInterval(this.iid);
+        });
+  $('#albumcover').live('mousedown', function(){
+      console.log('clicked');
+      this.iid && clearInterval(this.iid)
+      $("#jamieimg").attr("src", "images/gif-wireframe-rotate-illusion.gif");
+    }).live('mouseup', function() {
+      $("#jamieimg").attr("src", "images/jamieface.png");
+    });
+
+} else {
+
+  $('#albumcover').live('mousemove', function() {
+    Pixastic.process(document.getElementById('jamieimg'), "hsl", {
+      // saturation : 0,
+      // lightness : 0
+    });
+  });
+
+
+//
+//       $('#albumcover').live({
+//         mouseenter:
+//           function() {
+//             console.log("Mouse in");
+//             if ( !iid ){
+//               this._iid = setInterval(function() {
+//                   console.log(hue_val);
+//
+//                   Pixastic.process(document.getElementById('jamieimg'), "hsl", {
+//                     hue : 40
+//                     // saturation : 0,
+//                     // lightness : 0
+//                   });
+//
+//                   // hue_val = (hue_val + 1);
+//               }, 150);
+//
+//               iid = this._iid;
+//             }
+//           },
+//         mouseleave:
+//           function() {
+//             console.log("Mouse out");
+//             iid && clearInterval(iid);
+//             iid = null;
+//           }
+//       });
+
+}
 
