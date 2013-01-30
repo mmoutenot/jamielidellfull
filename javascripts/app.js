70773863abe762c5735b5715c344ac205142947a
Marshall <mmoutenot@gmail.com>
2013-01-30 00:08:56 -0500

    Modify site to meet Warp Records' specification for launch. Given only a few hours notice, so code is GROSS :(

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,16 +1,16 @@
-$(document).ready(function (){
-  var refreshID = setInterval(function(){
-    var currentHoleSize = 1.9;
-    var volume = 100;
-    var scrollPosition = $(document).scrollTop();
-    currentHoleSize = Math.max(1.9 - scrollPosition/190, 0.5);
-    volume = 100 - $(document).scrollTop()/7;
-    $('#page').css({'background-image':
-      '-webkit-radial-gradient(rgba(10, 10, 10, .1) '+currentHoleSize+'px, #111 1px)'});
-    if (window.player && $.isFunction(window.player.setVolume))
-      window.player.setVolume(volume);
-  }, 100);
-});
+// $(document).ready(function (){
+//   var refreshID = setInterval(function(){
+//     var currentHoleSize = 1.9;
+//     var volume = 100;
+//     var scrollPosition = $(document).scrollTop();
+//     currentHoleSize = Math.max(1.9 - scrollPosition/190, 0.5);
+//     volume = 100 - $(document).scrollTop()/7;
+//     $('#page').css({'background-image':
+//       '-webkit-radial-gradient(rgba(10, 10, 10, .1) '+currentHoleSize+'px, #111 1px)'});
+//     if (window.player && $.isFunction(window.player.setVolume))
+//       window.player.setVolume(volume);
+//   }, 100);
+// });
 
 $('#title').click(function (){
   event.preventDefault();
@@ -21,7 +21,8 @@ $(document).ready(function() {
   var windowHeight = $(window).height();
   console.log(windowHeight);
   $('#page').css({'margin-top': (windowHeight-155) + 'px'});
-  $('#mainVideo').tubular({videoId: 'mNY5jJv57MQ', mute: false, repeat: true, playButtonClass: 'tubular-shield'});
+  $('#mainVideo').css({'height': (windowHeight-155)});
+  // $('#mainVideo').tubular({videoId: 'mNY5jJv57MQ', mute: false, repeat: true, playButtonClass: 'tubular-shield'});
 
   var viewOnYoutubeHidden = false;
   var refreshID = setInterval(function() {
@@ -75,6 +76,60 @@ $(function() {
       $el
       .width(newWidth)
       .height(newWidth * $el.data('aspectRatio'));
+
+    });
+
+    // Kick off one resize to fix all videos on page load
+  }).resize();
+
+})
+
+// repeated code due to < 24 hour notice! BLAGH!
+$(function() {
+
+  // Find all YouTube videos
+  var $allVideos = $(".youtubeMain"),
+
+  // The element that is fluid width
+  $fluidEl = $("#mainVideo");
+
+  // Figure out and save aspect ratio for each video
+  $allVideos.each(function() {
+
+    $(this)
+    .data('aspectRatio', this.height / this.width)
+
+    // and remove the hard coded width/height
+    .removeAttr('height')
+    .removeAttr('width');
+
+    var windowHeight = $(window).height();
+    var newMargin = ((windowHeight-this.height)/4);
+    var marginBound = (windowHeight-155);
+    console.log(newMargin);
+    console.log(marginBound);
+    $('#mainVideo').css({'margin-top': Math.min(newMargin,marginBound)});
+
+  });
+
+  // When the window is resized
+  // (You'll probably want to debounce this)
+  $(window).resize(function() {
+
+    var newWidth = $fluidEl.width();
+
+    // Resize all videos according to their own aspect ratio
+    $allVideos.each(function() {
+
+      var windowHeight = $(window).height();
+      var heightBound = (windowHeight-235);
+      var $el = $(this);
+      $el
+      .width(newWidth)
+      .height(Math.min(newWidth * $el.data('aspectRatio'), heightBound));
+      var newMargin = ((windowHeight-$('.youtubeMain').height())/4);
+      console.log(newMargin);
+      $('#mainVideo').css({'margin-top': newMargin});
 
     });
 
