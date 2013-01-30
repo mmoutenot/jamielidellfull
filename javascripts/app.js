b7e5f982171ed2066e8b60f485bf2b78fa48a358
Marshall <mmoutenot@gmail.com>
2013-01-30 08:59:26 -0500

    add video at top, fluid with column

--- javascripts/app.js
+++ javascripts/app.js
@@ -134,6 +134,46 @@ $(function() {
 
 })
 
+// repeated code due to < 24 hour notice! BLAGH!
+$(function() {
+
+  // Find all YouTube videos
+  var $allVideos = $(".youtubeMobile"),
+
+  // The element that is fluid width
+  $fluidEl = $("#mobileVideoWrapper");
+
+  // Figure out and save aspect ratio for each video
+  $allVideos.each(function() {
+
+    $(this)
+    .data('aspectRatio', this.height / this.width)
+
+    // and remove the hard coded width/height
+    .removeAttr('height')
+    .removeAttr('width');
+
+  });
+
+  $(window).resize(function() {
+
+    var newWidth = $fluidEl.width();
+
+    // Resize all videos according to their own aspect ratio
+    $allVideos.each(function() {
+
+      var $el = $(this);
+      $el
+      .width(newWidth)
+      .height(newWidth * $el.data('aspectRatio'));
+
+    });
+
+    // Kick off one resize to fix all videos on page load
+  }).resize();
+
+})
+
 
 $(document).ready(function(){
   var hue_val = 0;
